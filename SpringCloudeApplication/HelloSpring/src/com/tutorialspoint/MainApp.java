2d05bb1bcc658546c20760ee389728db60e77a75
priti.banage@gmail.com <@smallwonder1>
2018-01-11 11:03:19 +0530

    Single statement

---
+++ SpringCloudeApplication/HelloSpring/src/com/tutorialspoint/MainApp.java
@@ -0,0 +1,19 @@
+package com.tutorialspoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+   public static void main(String[] args) {
+      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+      obj.getMessage();
+      
+      String mynae="abc";
+      System.out.println("Name after allocating "+mynae);
+   String myname1 =new String("abc");
+   
+   System.out.println("Name after allocating "+myname1);
+   System.out.println("Name after allocating "+myname1);
+   }
+}
